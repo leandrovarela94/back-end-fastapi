65761b70636fc34bad3262b1f6e3c27dffd38654
leandrovarela94 <leandro_varela@live.com>
2023-04-16 20:11:21 -0300

    AlfaZero - Redefined

--- sql/postgres.py
+++ sql/postgres.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 load_dotenv()
 
 database_postgres = os.environ["DATABASE"]
+host_postgres = os.environ["HOSTNAME_POSTGRES"]
 user_postgres = os.environ["USER_POSTGRES"]
 password_postgres = os.environ["PASSWORD_POSTGRES"]
 
@@ -15,7 +16,7 @@ class Postgres(BaseModel):
 
     def connect_postgres():
         conn = psycopg2.connect(
-            database=database_postgres, user=user_postgres, password=password_postgres)
+            database=database_postgres, host=host_postgres, user=user_postgres, password=password_postgres)
         cur = conn.cursor()
 
         return cur, conn
